5966baa0851ce51e763910c8b401eb2ca2f72ac6
jonescosta <jones.ncosta@gmail.com>
2026-04-22 11:36:00 -0300

    ajustes scripts

--- sql/create_profile.sql
+++ sql/create_profile.sql
@@ -1,4 +1,17 @@
 BEGIN
+DBMS_NETWORK_ACL_ADMIN.APPEND_HOST_ACE( 
+host => 'api.cohere.ai',
+lower_port => null,
+upper_port => null,
+ace => xs$ace_type(
+privilege_list => xs$name_list('connect', 'resolve'),
+principal_name => 'LANGFLOW',
+principal_type => xs_acl.ptype_db)
+);
+END;
+/
+
+BEGIN
   DBMS_CLOUD_AI.CREATE_PROFILE(
     profile_name => 'COHERE_PROFILE',
     attributes   => '{
