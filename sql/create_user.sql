58f4b886d9e331490bd3963e60eee00ceb8f784a
jonescosta <jones.ncosta@gmail.com>
2026-05-08 17:52:53 -0300

    tests

--- sql/create_user.sql
+++ sql/create_user.sql
@@ -31,3 +31,14 @@ END;
 
 -- QUOTA
 ALTER USER LANGFLOW QUOTA UNLIMITED ON DATA;
+
+
+
+BEGIN
+  DBMS_NETWORK_ACL_ADMIN.APPEND_HOST_ACE(
+    host => '*',
+    ace => xs$ace_type(privilege_list => xs$name_list('connect'),
+                       principal_name => 'LANGFLOW',
+                       principal_type => xs_acl.ptype_db));
+END;
+/
